8683765dc74d36db9e733e82c3baedecee73a989
firestrag <firestrag@gmail.com>
2016-03-18 07:49:03 +0100

    Zadanie
    Wcześniej był kod z laborków nr 3 pod tym

--- zad_na_prog_4.cpp
+++ zad_na_prog_4.cpp
@@ -143,26 +143,102 @@ void show(node* head){
     }
 }
 
+
+
 int main(){
 
-    node* head = NULL;
+    node* stos = NULL;
+    node* kolejka = NULL;
 
-    add(&head, 5, 0);
-    add(&head, 15, 0);
-    add(&head, 18, 1);
-    add(&head, 29, 3);
-    add(&head, 24, 3);
-    add(&head, 27, 3);
-    add(&head, 28, 3);
-    usun(&head, 0);
-    usun(&head, 3);
-    usun(&head, 4);
-    usun(&head, 4);
-    usun(&head, 0);
-    usun(&head, 0);
-    usun(&head, 0);
-    usun(&head, 0);
-    show(head);
+    for(;;){
+        int wybor = 0;
+        cout << "Wybierz:\n"
+        << "1 - aby dodac element do stosu\n"
+        << "2 - aby usunac element ze stosu\n"
+        << "3 - aby wyswietlic ilosc elementow na stosie\n"
+        << "4 - aby wyswietlic stos\n"
+        << "5 - aby wyswietlic informacje czy stos jest pusty\n"
+
+        << "11 - aby dodac element do kolejki\n"
+        << "12 - aby usunac element z kolejki\n"
+        << "13 - aby wyswietlic ilosc elementow w kolejce\n"
+        << "14 - aby wyswietlic kolejke\n"
+        << "15 - aby wyswietlic informacje czy kolejka jest pusta\n"
+
+        << "0 - aby zakonczyc dzialanie programu\n"
+        << "Wybor: ";
+
+        cin >> wybor;
+
+        switch(wybor){
+        case 0:
+            return 0;
+
+        case 1:
+            cout << "Wprowadz liczbe jaka chcesz dodac na stos:";
+            cin >> wybor;
+            add(&stos, wybor, size(stos));
+            break;
+        case 2:
+            if(!size(stos)){
+                cout << "Stos pusty!!!" << endl;
+                break;
+            }
+            usun(&stos, size(stos)-1);
+            break;
+        case 3:
+            cout << size(stos) << endl;
+            break;
+        case 4:
+            if(!size(stos)){
+                cout << "Stos pusty"<< endl;
+                break;
+            }
+            show(stos);
+            break;
+        case 5:
+            if(size(stos)){
+                cout << "Stos nie jest pusty"<< endl;
+            }else{
+                cout << "Stos jest pusty" << endl;
+            }
+            break;
+
+        case 11:
+            cout << "Wprowadz liczbe jaka chcesz dodac do kolejki:";
+            cin >> wybor;
+            add(&kolejka, wybor, size(kolejka));
+            break;
+        case 12:
+            if(!size(kolejka)){
+                cout << "Kolejka pusta!!!" << endl;
+                break;
+            }
+            usun(&kolejka, 0);
+            break;
+        case 13:
+            cout << size(kolejka) << endl;
+            break;
+        case 14:
+            if(!size(kolejka)){
+                cout << "Kolejka pusta"<< endl;
+                break;
+            }
+            show(kolejka);
+            break;
+        case 15:
+            if(size(kolejka)){
+                cout << "Kolejka nie jest pusta"<< endl;
+            }else{
+                cout << "Kolejka jest pusta" << endl;
+            }
+            break;
+
+        default:
+            cout << "Zostala wybrana bledna opcja!!!" << endl;
+        }
+
+    }
 
 
     return 0;
